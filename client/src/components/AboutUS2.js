ee200f4f67c7222681a79df7b188daf3ea5486e7
nandisha.d@think41.com <nandisha.d@think41.com>
2024-09-04 11:02:01 +0530

    Debugged About Us Section

--- client/src/components/AboutUS2.js
+++ client/src/components/AboutUS2.js
@@ -1,63 +1,31 @@
 import React from "react";
-import { motion } from "framer-motion";
 
 import MeetFounders from "./AboutUsComps/MeetFounders";
 import TimelineComp from "./AboutUsComps/TimeLine";
 import WhoAndWhat2 from "./AboutUsComps/WhoandWhat2";
 import CompStats from "./AboutUsComps/CompanyStats";
-import Hero from "./AboutUsComps/AboutHero2";
-// import AboutHero from "./AboutUsComps/AboutHero";
-
-
-const containerVariants = {
-  hidden: { opacity: 0, y: 20 },
-  visible: { opacity: 1, y: 0 },
-};
+// import Hero from "./AboutUsComps/AboutHero2";
+import AboutHero from "./AboutUsComps/AboutHero";
 
 function AboutUS2() {
   return (
     <>
       {/* Your content */}
-      <div className="relative space-y-8">
-        <Hero />
+      <div className="relative space-y-8 mt-20">
+        <AboutHero />
         <CompStats />
-        <motion.div
-          id="mission-vision"
-          initial="hidden"
-          animate="visible"
-          variants={containerVariants}
-          transition={{ duration: 1, ease: "easeInOut" }}
-        >
+        <div id="mission-vision">
           <WhoAndWhat2 />
-        </motion.div>
-        <motion.div
-          id="t41-advantage"
-          initial="hidden"
-          animate="visible"
-          variants={containerVariants}
-          transition={{ duration: 1, ease: "easeInOut" }}
-        >
-          
+        </div>
+        <div id="t41-advantage">
           <MeetFounders />
-        </motion.div>
-        <motion.div
-          id="leadership"
-          initial="hidden"
-          animate="visible"
-          variants={containerVariants}
-          transition={{ duration: 1, ease: "easeInOut" }}
-        >
+        </div>
+        {/* <div id="leadership">
           <TimelineComp />
-        </motion.div>
-        <motion.div
-          id="our-journey"
-          initial="hidden"
-          animate="visible"
-          variants={containerVariants}
-          transition={{ duration: 1, ease: "easeInOut" }}
-        >
+        </div> */}
+        <div id="our-journey">
           {/* Add the component or content related to "Our Journey" here */}
-        </motion.div>
+        </div>
       </div>
     </>
   );
