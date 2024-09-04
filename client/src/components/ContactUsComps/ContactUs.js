222d1353f2860a6de973a1a644311faecc4da9b4
nandisha.d@think41.com <nandisha.d@think41.com>
2024-09-05 00:02:05 +0530

    Added Dropbox effect for contact us page

--- client/src/components/ContactUsComps/ContactUs.js
+++ client/src/components/ContactUsComps/ContactUs.js
@@ -39,11 +39,11 @@ function ContactUs() {
             placeholder="Telephone"
             className="bg-gray-200 border-none rounded-lg p-3 mb-2 w-full outline-none"
           />
-          <input
-            type="text"
+          <textarea
             placeholder="Messages"
-            className="bg-gray-200 border-none rounded-lg p-3 mb-2 w-full outline-none"
-          />
+            className="w-full h-40 p-3 mb-2 bg-gray-200 rounded-lg outline-none resize-none overflow-y-auto "
+          ></textarea>
+
           <button className="bg-purple-700 text-white px-10 py-2 rounded-lg font-semibold mt-4">
             Submit
           </button>
@@ -83,10 +83,39 @@ function ContactUs() {
             placeholder="Country"
             className="bg-gray-200 border-none rounded-lg p-3 mb-2 w-full outline-none"
           />
-          <input
-            type="file"
-            className="bg-gray-200 border-none rounded-lg p-3 mb-2 w-full outline-none"
-          />
+          <div className="flex items-center justify-center w-full">
+            <label
+              htmlFor="file-upload"
+              className="flex flex-col items-center justify-center w-full h-40 border-2 border-dashed border-gray-300 rounded-lg cursor-pointer bg-gray-200 hover:bg-gray-300"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg
+                  aria-hidden="true"
+                  className="w-10 h-10 mb-3 text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M7 16V9a4 4 0 014-4h2a4 4 0 014 4v7m-5 4v-2a2 2 0 00-2-2h-1a2 2 0 00-2 2v2m8 0H7"
+                  ></path>
+                </svg>
+                <p className="mb-2 text-sm text-gray-500">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-gray-500">
+                  SVG, PNG, JPG or GIF (MAX. 800x400px)
+                </p>
+              </div>
+              <input id="file-upload" type="file" className="hidden" />
+            </label>
+          </div>
+
           <button className="bg-purple-700 text-white px-10 py-2 rounded-lg font-semibold mt-4">
             Submit
           </button>
